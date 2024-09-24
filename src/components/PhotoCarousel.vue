95c9a04e3264fe081d65b009d4846befdf05acd5
nathandickinson32 <nathandickinson32@gmail.com>
2024-09-24 10:49:02 -0400

    added a photo carousel, needs styled

---
+++ src/components/PhotoCarousel.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="content">
+    <div class="myCarousel">
+        <transition name="fade">
+          <img :src="randomImage1" class="top-right-image" :key="randomIndex1" />
+        </transition>
+    </div>
+  </div>
+</template>
+
+<script>
+import fiencee2 from '../assets/images/fiencee2.jpg'
+import cleBugImage from '../assets/images/cleBug.jpg'
+import meMowImage from '../assets/images/me&mow.jpg'
+export default {
+data(){
+    return{
+        randomIndex1: 0,
+intervalId: null,
+images: [
+        // fiencee2,
+        // cleBugImage,
+        // meMowImage
+
+        
+      ],
+    }
+},
+computed: {
+    randomImage1() {
+      return this.images[this.randomIndex1];
+    },
+},
+mounted(){
+    this.intervalId = setInterval(() => {
+      this.randomIndex1 = Math.floor(Math.random() * this.images.length);
+    
+    }, 5000); 
+}
+}
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="content">
     <h1>Homepage</h1>
-
+<photo-carousel/>
 </div>
 </template>
 
 <script>
+import PhotoCarousel from '../components/PhotoCarousel.vue';
 export default {
-
+components:{
+  PhotoCarousel
+}
 }
 </script>
 
